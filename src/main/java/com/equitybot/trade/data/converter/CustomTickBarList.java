29fc2251ac36ba7761de6d3b2d35c5579ff55e89
lalit1980@gmail.com <Manager123$>
2018-07-09 22:10:09 +0800

    test

--- src/main/java/com/equitybot/trade/data/converter/CustomTickBarList.java
+++ src/main/java/com/equitybot/trade/data/converter/CustomTickBarList.java
@@ -11,8 +11,13 @@ import java.time.ZonedDateTime;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.Ignition;
+import org.apache.ignite.cache.CacheAtomicityMode;
+import org.apache.ignite.cache.CacheMode;
+import org.apache.ignite.cache.CacheRebalanceMode;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
+import org.apache.ignite.spi.discovery.tcp.ipfinder.kubernetes.TcpDiscoveryKubernetesIpFinder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,9 +60,22 @@ public class CustomTickBarList {
 		this.workingTickBarMap = new HashMap<>();
 		this.timeSeriesMap = new HashMap<>();
 		IgniteConfiguration cfg = new IgniteConfiguration();
+		TcpDiscoverySpi tcpDiscoverySpi= new TcpDiscoverySpi();
+		TcpDiscoveryKubernetesIpFinder ipFinder  = new TcpDiscoveryKubernetesIpFinder();
+		ipFinder.setServiceName("ignite");
+		tcpDiscoverySpi.setIpFinder(ipFinder);
+		cfg.setDiscoverySpi(tcpDiscoverySpi);
+		cfg.setPeerClassLoadingEnabled(true);
+		cfg.setClientMode(true);
 		Ignite ignite = Ignition.start(cfg);
 		Ignition.setClientMode(true);
+		ignite.active(true);
 		CacheConfiguration<String, TimeSeries> ccfg = new CacheConfiguration<String, TimeSeries>("TimeSeriesCache");
+		ccfg.setAtomicityMode(CacheAtomicityMode.ATOMIC);
+		ccfg.setCacheMode(CacheMode.PARTITIONED);
+		ccfg.setRebalanceMode(CacheRebalanceMode.NONE);
+		ccfg.setDataRegionName("1GB_Region");
+		ccfg = new CacheConfiguration<String, TimeSeries>("TimeSeriesCache");
 		this.cache = ignite.getOrCreateCache(ccfg);
 	}
 
